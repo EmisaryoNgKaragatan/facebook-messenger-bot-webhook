400a34a15f6e618f26cd920db258cd1ad4a79b93
EmisaryoNgKaragatan <EmisaryoNgKaragatan@users.noreply.github.com>
2020-12-19 20:46:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
-const dotenv = require('dotenv');
-dotenv.config();
 
 app.use(bodyParser.json());
 
